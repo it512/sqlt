8ed6952362864cc4665ba5bbe7c182107d6c5f6b
mikewang <mikewang@debian.mikewang.info>
2017-03-20 12:07:34 +0800

    add log and use define function supports

--- tpl.go
+++ tpl.go
@@ -2,22 +2,37 @@ package sqlt
 
 import (
 	"bytes"
+	"log"
 	"text/template"
 )
 
 type (
 	DefaultSqlLoader struct {
 		t *template.Template
+		l *log.Logger
 	}
 )
 
 func (l *DefaultSqlLoader) LoadSql(id string, param interface{}) (string, int, error) {
 	buffer := new(bytes.Buffer)
 	e := l.t.ExecuteTemplate(buffer, id, param)
-	return buffer.String(), 0, e
+	sql := buffer.String()
+
+	if l.l != nil {
+		if e != nil {
+			l.l.Panicln(e)
+		}
+		l.l.Println(sql, param)
+	}
+	return sql, 0, e
 }
 
 func NewDefaultSqlLoader(pattern string) *DefaultSqlLoader {
+	return NewSqlLoader(pattern, nil, make(template.FuncMap))
+}
+
+func NewSqlLoader(pattern string, l *log.Logger, funcMap template.FuncMap) *DefaultSqlLoader {
 	tpl := template.Must(template.ParseGlob(pattern))
-	return &DefaultSqlLoader{t: tpl}
+	tpl.Funcs(funcMap)
+	return &DefaultSqlLoader{t: tpl, l: l}
 }
